de5279c33e7a7f01632019a3817f8acdb4a76ee7
Akira-uestc <akira.uestc@gmail.com>
2024-06-06 18:10:10 +0800

    waybar random wallpaper

---
+++ waybar/random.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# 检测进程是否存在
+if pgrep -x "swaybg" >/dev/null; then
+	# 进程存在，杀死进程
+	pkill -x "swaybg"
+	echo "进程已被杀死"
+else
+	# 进程不存在，不做任何事
+	echo "进程不存在，无需杀死"
+fi
+nohup swaybg -i $(find /home/akira/Pictures/swaybg/ -type f | shuf -n 1) -m fill >/dev/null 2>&1 &
