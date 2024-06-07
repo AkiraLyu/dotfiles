d5d60815c1309c2aa0e2cc5235b5a3d9ff1d6db3
Akira-uestc <akira.uestc@gmail.com>
2024-06-08 00:56:15 +0800

    fish initial config

---
+++ fish/config.fish
@@ -0,0 +1,5 @@
+set -g __fish_terminal_just_started 1
+
+oh-my-posh init fish --config ~/.config/fish/myTheme.json | source
+
+#oh-my-posh init fish --config ~/.poshthemes/clean-detailed.omp.json | source
