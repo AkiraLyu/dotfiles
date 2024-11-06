b143abdcb13cd169fb531d274bd8b3d3e8753ac0
Akira-uestc <akira.uestc@gmail.com>
2024-11-06 18:45:02 +0800

    feat: synchronize sway and hyprland configuration.

--- fish/conf.d/sway.fish
+++ fish/conf.d/sway.fish
@@ -1,5 +1,6 @@
-export SDL_VIDEODRIVER=wayland
-export _JAVA_AWT_WM_NONREPARENTING=1
-export QT_QPA_PLATFORM=wayland
-export XDG_CURRENT_DESKTOP=sway
-export XDG_SESSION_DESKTOP=sway
+#export SDL_VIDEODRIVER=wayland
+#export _JAVA_AWT_WM_NONREPARENTING=1
+#export QT_QPA_PLATFORM=wayland
+#export XDG_CURRENT_DESKTOP=hyprland
+#export XDG_SESSION_DESKTOP=hyprland
+export AQ_NO_MODIFIERS=1
--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -107,7 +107,7 @@ vim.api.nvim_create_autocmd("BufWritePost", {
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "c" },
+  pattern = { "c", "cpp" },
   callback = function()
     vim.b.autoformat = false
   end,
