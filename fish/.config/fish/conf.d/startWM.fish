79ad199e86afefb11bcdb23d8f24d42a8ecf10c7
Akira-uestc <akira.uestc@gmail.com>
2025-04-21 18:11:30 +0800

    feat: use gnu stow to manage dotfiles.

---
+++ fish/.config/fish/conf.d/startWM.fish
@@ -0,0 +1,19 @@
+set Start_Env kde
+export XDG_CURRENT_DESKTOP=$Start_Env
+
+set TTY1 (tty)
+if [ "$TTY1" = /dev/tty1 ]
+
+    if [ "$Start_Env" = hypr ]
+        exec Hyprland
+    else if [ "$Start_Env" = sway ]
+        exec sway
+    else if [ "$Start_Env" = wf ]
+        exec wayfire
+    else if [ "$Start_Env" = kde ]
+        exec /usr/lib/plasma-dbus-run-session-if-needed /usr/bin/startplasma-wayland
+    else if [ "$Start_Env" = cosmic ]
+        exec start-cosmic
+    end
+
+end
