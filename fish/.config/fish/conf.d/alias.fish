2a50999244e29354f1059eea9d04f3ab1fac9deb
Akira-uestc <akira.uestc@gmail.com>
2025-04-24 16:55:37 +0800

    fish: optimize configuration file structure

---
+++ fish/.config/fish/conf.d/alias.fish
@@ -0,0 +1,7 @@
+alias n=neofetch
+alias l="ls -l"
+alias vim=nvim
+alias chat="python ~/.local/scripts/deepseek.py"
+alias snap="~/.local/scripts/snaps.sh create"
+alias cat=bat
+alias ls="ls -a --color=auto"
---
+++ fish/.config/fish/conf.d/env.fish
@@ -0,0 +1,8 @@
+export QT_SCREEN_SCALE_FACTORS=1
+export QT_FONT_DPI=120
+export LIBVA_DRIVER_NAME=iHD
+export LFS=/mnt/lfs
+export EDITOR=nvim
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/bin:/home/akira/.cargo/bin:/home/akira/.nix-profile/bin
+export QT_XCB_GL_INTEGRATION=none
+export CRYPTOGRAPHY_OPENSSL_NO_LEGACY=none
