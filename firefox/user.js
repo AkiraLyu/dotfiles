24b52d81fecf6a2db179eca1754c0de0d697f011
Akira-uestc <akira.uestc@gmail.com>
2025-04-24 16:54:41 +0800

    firefox: new frontpage and better user.js

---
+++ firefox/user.js
@@ -0,0 +1,7 @@
+user_pref("layout.css.devPixelsPerPx", "1.25");
+user_pref("mousewheel.default.delta_multiplier_y", 80);
+user_pref("widget.use-xdg-desktop-portal.file-picker", 1);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.startup.homepage", "file:///home/akira/dotfiles/firefox/frontpage/index.html");
+user_pref("widget.gtk.rounded-bottom-corners.enabled", true);
+user_pref("sidebar.revamp", false);
