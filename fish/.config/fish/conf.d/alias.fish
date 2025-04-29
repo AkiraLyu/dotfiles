91a6e95f3b3160c1e9c95ef8d1d84dbd70637459
Akira-uestc <akira.uestc@gmail.com>
2025-04-30 01:59:17 +0800

    tools updates

--- fish/.config/fish/conf.d/alias.fish
+++ fish/.config/fish/conf.d/alias.fish
@@ -1,6 +1,7 @@
 alias n=neofetch
 alias l="ls -l"
-alias vim=nvim
+alias vim=/bin/nvim
+alias vi=/bin/vim
 alias chat="python ~/.local/scripts/deepseek.py"
 alias snap="~/.local/scripts/snaps.sh create"
 alias cat=bat
