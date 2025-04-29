66fee0c36a2dc593e75f3734136981ef66912635
Akira-uestc <akira.uestc@gmail.com>
2025-04-30 02:39:15 +0800

    nvim: better toggle for input method.

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -25,26 +25,46 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   end,
 })
 
--- 自动切换输入法
--- 切换到英文输入的函数
-local function switch_to_english()
-  os.execute("fcitx5-remote -c") -- 切换到默认（英文）输入法
-end
-
--- 自动切换输入法的 autocmd
-vim.api.nvim_create_autocmd("InsertLeave", {
-  callback = switch_to_english, -- 在离开插入模式时调用
-})
-
 -- 映射一个快捷键手动触发
 vim.api.nvim_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true, silent = true })
 
--- 自动触发光标悬浮显示
--- vim.api.nvim_create_autocmd("CursorHold", {
---   pattern = "*",
---   callback = function()
---     vim.lsp.buf.hover()
---   end,
--- })
---
---
+-- 保存当前输入法状态的变量
+local input_status = 0
+
+-- 检查是否是 Linux 且有 fcitx5-remote 命令
+local function has_fcitx5()
+  return vim.fn.executable("fcitx5-remote") == 1
+end
+
+-- 获取当前输入法状态
+local function get_input_status()
+  return tonumber(vim.fn.system("fcitx5-remote")) or 0
+end
+
+-- 切换到英文输入法
+local function switch_to_english()
+  if has_fcitx5() then
+    input_status = get_input_status()
+    if input_status ~= 0 then
+      vim.fn.system("fcitx5-remote -c")
+    end
+  end
+end
+
+-- 恢复原来的输入法状态
+local function restore_input_method()
+  if has_fcitx5() and input_status == 2 then
+    vim.fn.system("fcitx5-remote -o")
+  end
+end
+
+-- 设置自动命令
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = "*",
+  callback = switch_to_english,
+})
+
+vim.api.nvim_create_autocmd("InsertEnter", {
+  pattern = "*",
+  callback = restore_input_method,
+})
