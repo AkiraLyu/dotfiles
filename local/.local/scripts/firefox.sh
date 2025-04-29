06e81ebc9e2e71f60e6b94e974ca6f455d56c407
Akira-uestc <akira.uestc@gmail.com>
2025-04-30 01:56:45 +0800

    firefox: add a script to let stow create softlinks in the right place. remove ShyFox configuration

---
+++ local/.local/scripts/firefox.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 自动找出 Firefox 的默认 profile 目录
+profile_dir=$(find ~/.mozilla/firefox -maxdepth 1 -type d -name "*.default-release" | head -n 1)
+
+if [ -z "$profile_dir" ]; then
+    echo "未找到 Firefox profile 目录"
+    exit 1
+fi
+
+stow -d ~/dotfiles -t "$profile_dir" firefox
