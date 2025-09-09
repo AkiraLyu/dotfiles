aaf414ad5e07f088b6b9096eca9b86d990bd9ba3
Akira-uestc <akira.uestc@gmail.com>
2025-09-09 13:00:43 +0800

    fish: switch font between niri and kde

--- fish/.config/fish/conf.d/startWM.fish
+++ fish/.config/fish/conf.d/startWM.fish
@@ -1,15 +1,27 @@
 set Start_Env niri
 
-set TTY1 (tty)
-if [ "$TTY1" = /dev/tty1 ]
+function switch_font
+    if test "$Start_Env" = "kde"
+        sed -i 's/Noto Sans CJK SC,/Noto Sans,/' ~/.config/qt6ct/qt6ct.conf
+    else if test "$Start_Env" = "niri"
+        sed -i 's/Noto Sans,/Noto Sans CJK SC,/' ~/.config/qt6ct/qt6ct.conf
+    end
+end
 
-    if [ "$Start_Env" = hypr ]
+
+set TTY1 (tty)
+
+if test "$TTY1" = "/dev/tty1"
+
+    if test "$Start_Env" = "hypr"
         exec Hyprland
-    else if [ "$Start_Env" = sway ]
+    else if test "$Start_Env" = "sway"
         exec sway
-    else if [ "$Start_Env" = kde ]
+    else if test "$Start_Env" = "kde"
+        switch_font
         exec /usr/lib/plasma-dbus-run-session-if-needed /usr/bin/startplasma-wayland
-    else if [ "$Start_Env" = niri ]
+    else if test "$Start_Env" = "niri"
+        switch_font
         exec niri --session
     end
 
