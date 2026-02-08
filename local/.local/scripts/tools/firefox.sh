998e3236d34eccdee4cd7a9d271f8094e743eef3
Akira-uestc <akira.uestc@gmail.com>
2026-02-08 11:32:11 +0800

    reinstall archlinux so the pkglist only include implict installed packages.

--- local/.local/scripts/tools/firefox.sh
+++ local/.local/scripts/tools/firefox.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 自动找出 Firefox 的默认 profile 目录
-profile_dir=$(find ~/.mozilla/firefox -maxdepth 1 -type d -name "*.default-release" | head -n 1)
+profile_dir=$(find ~/.config/mozilla/firefox -maxdepth 1 -type d -name "*.default-release" | head -n 1)
 
 if [ -z "$profile_dir" ]; then
     echo "未找到 Firefox profile 目录"
